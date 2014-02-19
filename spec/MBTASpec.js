f69c10bd803c3f9b7b8c467f771be21cb5e9d627
Janice Smith <janice.k.n.smith@gmail.com>
2014-02-19 18:57:11 -0500

    now have starting station passing

--- spec/MBTASpec.js
+++ spec/MBTASpec.js
@@ -1,23 +1,32 @@
 describe("SubwayTrip", function() {
   var trip1;
   var trip2;
+  var trip3;
 
   beforeEach(function() {
     trip1 = new SubwayTrip('green', 'park st', 'red', 'alewife');
     trip2 = new SubwayTrip('orange', 'forest hills', 'red', 'alewife');
+    trip3 = new SubwayTrip('red', 'alewife', 'red', 'south station');
   });
 
   it("SubwayTrip trip to have a start line", function() {
      expect(trip1.startingLine).toEqual("green");
      expect(trip2.startingLine).toEqual("orange");
+     expect(trip3.startingLine).toEqual("red");
   });
 
+  it("SubwayTrip trip to have a start station", function() {
+     expect(trip1.startingStation).toEqual("park st");
+     expect(trip2.startingStation).toEqual("forest hills");
+     expect(trip3.startingStation).toEqual("alewife");
+  });
 
   describe("when the function distance is called", function() {
 
     it("should return the number of stops", function() {
       expect(trip1.distance()).toEqual(6);
       expect(trip2.distance()).toEqual(11);
+      expect(trip3.distance()).toEqual(7);
     });
   });
 
