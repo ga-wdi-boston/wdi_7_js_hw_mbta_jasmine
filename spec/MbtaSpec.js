cf5288648b6d0de68691e40e1688fa281827ec98
Prescott Murphy <prurph@gmail.com>
2014-02-19 18:51:00 -0500

    Add specs and code to check to ensure line/stop are valid combinations

--- spec/MbtaSpec.js
+++ spec/MbtaSpec.js
@@ -1,25 +1,63 @@
 describe("Mbta", function() {
-  var kendallToHayGreen,
+  var stdTrip,
       tooShortTrip,
-      tooLongTrip;
+      tooLongTrip,
+      invalidLine,
+      invalidStop;
 
   beforeEach(function() {
-    kendallToHayGreen = new Mbta.Trip("red", "kendall", "green", "haymarket")
+    stdTrip = new Mbta.Trip("red", "kendall", "green", "haymarket")
   });
 
   describe("Mbta.Trip constructor", function() {
     it("should be initialized with 4 attributes for starting/ending", function() {
-      expect(kendallToHayGreen.startingLine).toEqual("red");
-      expect(kendallToHayGreen.startingStation).toEqual("kendall");
-      expect(kendallToHayGreen.endingLine).toEqual("green");
-      expect(kendallToHayGreen.endingStation).toEqual("haymarket");
+      expect(stdTrip.startingLine).toBeValid;
+    });
+    it("should store starting/ending as from and to object attributes", function() {
+      expect(stdTrip.startHere).toEqual({line: "red", station: "kendall"});
+      expect(stdTrip.endHere).toEqual({line: "green", station: "haymarket"});
     });
     it("should throw an error when initialized with wrong number of args", function() {
       expect(function() {
         tooShortTrip = new Mbta.Trip("red", "kendall"); }).toThrowError("Wrong number of args");
       expect(function() {
         tooLongTrip = new Mbta.Trip("red", "kendall", "red", "park street",
-          "green", "haymarket"); }).toThrowError("Wrong number of args");
+          "Green", "Haymarket"); }).toThrowError("Wrong number of args");
     });
+    it("should throw an error when initialized with an invalid line", function() {
+      expect(function() {
+        invalidLine = new Mbta.Trip("red", "kendall", "blue", "government center"); }
+        ).toThrowError("Invalid line name");
+      expect(function() {
+        invalidStop = new Mbta.Trip("red", "kendall", "red", "boylston"); }
+        ).toThrowError("Invalid stop name");
+    });
+  });
+  describe("Mbta.Trip.prototype", function() {
+    it("should give Trip objects lines object with line keys/stop arrays", function() {
+      expect(stdTrip.lines.green).toEqual(["haymarket",
+                                           "government center",
+                                           "park street",
+                                           "boylston",
+                                           "arlington",
+                                           "copley"]);
+      expect(stdTrip.lines.red).toEqual(["south station",
+                                         "park street",
+                                         "kendall",
+                                         "central",
+                                         "harvard",
+                                         "porter",
+                                         "davis",
+                                         "alewife"]);
+      expect(stdTrip.lines.orange).toEqual(["north station",
+                                            "haymarket",
+                                            "park street",
+                                            "state",
+                                            "downtown crossing",
+                                            "chinatown",
+                                            "back bay",
+                                            "forest hills"]);
+    });
+    it(".distanceToPark(place)")
   });
 });
--- src/Mbta.js
+++ src/Mbta.js
@@ -1,11 +1,42 @@
 var Mbta = {
   Trip: function(startingLine, startingStation, endingLine, endingStation) {
+    // Check argument length
     if (arguments.length !== 4) {
       throw new Error("Wrong number of args");
     }
-    this.startingLine = startingLine;
-    this.startingStation = startingStation;
-    this.endingLine = endingLine;
-    this.endingStation = endingStation;
+    if (!(startingLine in this.lines) || !(endingLine in this.lines)) {
+      throw new Error("Invalid line name");
+    } else if ((this.lines[startingLine].indexOf(startingStation) < 0) ||
+               (this.lines[endingLine].indexOf(endingStation) < 0)) {
+      throw new Error("Invalid stop name");
+    }
+
+    this.startHere = {line: startingLine, station: startingStation};
+    this.endHere = {line: endingLine, station: endingStation};
   }
 }
+
+Mbta.Trip.prototype.lines = {
+  'green':  ["haymarket",
+             "government center",
+             "park street",
+             "boylston",
+             "arlington",
+             "copley"],
+  'red':    ["south station",
+             "park street",
+             "kendall",
+             "central",
+             "harvard",
+             "porter",
+             "davis",
+             "alewife"],
+  'orange': ["north station",
+             "haymarket",
+             "park street",
+             "state",
+             "downtown crossing",
+             "chinatown",
+             "back bay",
+             "forest hills"]
+};
