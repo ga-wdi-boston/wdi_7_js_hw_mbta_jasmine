e4780796b90168d92864d03a0a87d89c0a38f82a
Janice Smith <janice.k.n.smith@gmail.com>
2014-02-19 18:59:53 -0500

    cleaning up var statment - also passing ending line

--- spec/MBTASpec.js
+++ spec/MBTASpec.js
@@ -1,12 +1,14 @@
 describe("SubwayTrip", function() {
-  var trip1;
-  var trip2;
-  var trip3;
+  var trip1,
+      trip2,
+      trip3,
+      trip4;
 
   beforeEach(function() {
     trip1 = new SubwayTrip('green', 'park st', 'red', 'alewife');
     trip2 = new SubwayTrip('orange', 'forest hills', 'red', 'alewife');
     trip3 = new SubwayTrip('red', 'alewife', 'red', 'south station');
+    trip4 = new SubwayTrip('red', 'alewife', 'green', 'park st');
   });
 
   it("SubwayTrip trip to have a start line", function() {
@@ -19,6 +21,12 @@ describe("SubwayTrip", function() {
      expect(trip1.startingStation).toEqual("park st");
      expect(trip2.startingStation).toEqual("forest hills");
      expect(trip3.startingStation).toEqual("alewife");
+  });
+
+  it("SubwayTrip trip to have an ending line", function() {
+     expect(trip1.endingLine).toEqual("red");
+     expect(trip2.endingLine).toEqual("red");
+     expect(trip4.endingLine).toEqual("green");
   });
 
   describe("when the function distance is called", function() {
