bd0dba74831366e981cf78312f6a8397bd3e04a5
K Mak <karen.mak@stx.oxon.org>
2014-02-19 23:21:24 -0500

    specs passed for numOfStops function

--- spec/mbtaSpec.js
+++ spec/mbtaSpec.js
@@ -2,22 +2,22 @@ describe('MBTA', function() {
   var journey;
 
   beforeEach(function() {
-    journey = new MBTA.Journey('alewife', 'red', 'green', 'arlington');
+    journey = new MBTA.Journey('alewife', 'red', 'red', 'porter');
   });
 
     describe('Journey', function() {
       it('should expect the location points to be set', function() {
       expect(journey.startingStation).toEqual('alewife');
       expect(journey.startingLine).toEqual('red');
-      expect(journey.endingStation).toEqual('arlington');
-      expect(journey.endingLine).toEqual('green');
+      expect(journey.endingStation).toEqual('porter');
+      expect(journey.endingLine).toEqual('red');
     });
   });
 
-  //   describe('numOfStops', function() {
-  //     it('should return the number of stops between stations via Park St', function() {
-  //     expect(MBTA.Journey.prototype.numOfStops(journey)).toEqual(8);
-  //   });
-  // });
+    describe('numOfStops', function() {
+      it('should return the number of stops between stations via Park St', function() {
+      expect(journey.numOfStops()).toEqual(2);
+    });
+  });
 });
 
--- src/mbta.js
+++ src/mbta.js
@@ -1,20 +1,19 @@
 var MBTA = {
+  lines: {
+    'green':['haymarket', 'government center', 'park st', 'bolyston', 'arlington', 'copley'],
+    'red': ['south station', 'park st', 'kendall', 'central', 'harvard', 'porter', 'davis', 'alewife'],
+    'orange': ['north station', 'haymarket', 'park st', 'state', 'downtown crossing', 'chinatown', 'back bay', 'forest hills']
+    },
   Journey: function(startingStation, startingLine, endingLine, endingStation) {
     this.startingStation = startingStation;
     this.startingLine = startingLine;
     this.endingLine = endingLine;
     this.endingStation = endingStation;
-  },
-
-  lines: {
-    'green':['haymarket', 'government center', 'park st', 'bolyston', 'arlington', 'copley'],
-    'red': ['south station', 'park st', 'kendall', 'central', 'harvard', 'porter', 'davis', 'alewife'],
-    'orange': ['north station', 'haymarket', 'park st', 'state', 'downtown crossing', 'chinatown', 'back bay', 'forest hills']
   }
 };
 
-// MBTA.Journey.prototype.numOfStops = function() {
-//     startToIntersection = Math.abs(MBTA.Journey.lines[this.startingLine].indexOf(this.startingStation) - MBTA.Journey.lines[this.startingLine].indexOf('Park Street'));
-//     endToIntersection = Math.abs(MBTA.Journey.lines[this.endingLine].indexOf(this.endingStation) - MBTA.Journey.lines[this.endingLine].indexOf('Park Street'));
-//     return startToIntersection + endToIntersection
-// }
+MBTA.Journey.prototype.numOfStops = function(journey) {
+  var startToIntersection = Math.abs(MBTA.lines[this.startingLine].indexOf(this.startingStation) - MBTA.lines[this.startingLine].indexOf('Park Street'));
+  var endToIntersection = Math.abs(MBTA.lines[this.endingLine].indexOf(this.endingStation) - MBTA.lines[this.endingLine].indexOf('Park Street'));
+  return (startToIntersection - endToIntersection);
+};
