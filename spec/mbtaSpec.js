04a3b1e11bf0d38d01ae472cf8cef407fbbe0c71
Becky Bell <rlbell@alum.bu.edu>
2014-02-20 08:55:39 -0500

    added function Route to MBTA variable

--- spec/mbtaSpec.js
+++ spec/mbtaSpec.js
@@ -2,12 +2,12 @@ describe('Route', function () {
 	var routeA, routeB;
 
   beforeEach(function() {
-    routeA = new Route ("green", "haymarket", "park st");
-    routeB = new Route ("red", "green", "haymarket", "alewife");
+    routeA = new MBTA.Route ("green", "green" "haymarket", "park st");
+    routeB = new MBTA.Route ("red", "green", "haymarket", "alewife");
   });
 
   it("has a line, startingStation, endingStation", function() {
-  	 expect(routeA.line).toEqual("green");
+  	 expect(routeA.startingLine).toEqual("green");
   	 expect(routeA.startingStation).toEqual("haymarket");
   	 expect(routeA.endingStation).toEqual("park st");
   });
@@ -20,8 +20,8 @@ describe('Route', function () {
   });
 
   it("has a number of stops between starting and ending stop", function() {
-  	expect(routeA.enter_exit_same_line).toEqual(2)
-  	expect(routeB.enter_exit_different_lines).toEqual()
+  	expect(routeA.distance_same_line).toEqual(2)
+  	expect(routeB.distance_different_lines).toEqual(7)
 	});
 
 });
--- src/mbta.js
+++ src/mbta.js
@@ -5,22 +5,36 @@ var MBTA = {
 	  'orange': ['north station', 'haymarket', 'park st', 'state', 'downtown crossing', 'chinatown', 'back bay', 'forest hills']
 	},
 
-		var startingLine,
-				startingStation,
-				endingLine,
-				endingStation,
-				lines,
-				numStops;
+	Route: function(startingLine, startingStation, endingLine, endingStation) {
+		this.startingLine;
+		this.startingStation;
+		this.endingLine;
+		this.endingStation;
+	},
 
+	MBTA.Route.prototype.distance_same_line = function () {
+		var	this.startingLine,
+			this.startingStation,
+			this.endingStation,
+		return enter_exit_same_line;
+	},
+
+	MBTA.Route.prototype.distance_different_lines = function () {
+		var	this.startingLine,
+			this.startingStation,
+			this.endingStation,
+		return enter_exit_different_lines;
+
+	},
 
 	enter_exit_same_line: function(line, startingStation, endingStation) {
 		var stops_array,
 				switch_one,
 				switch_two;
 		this.stops_array = this.lines[line];
-		this.switch_one = this.stops_array.indexOf(startingStation);
-		this.switch_two = this.stops_array.indexOf(endingStation);
-		abs_stop = Math.abs(switch_one - switch_two);
+		this.switch_one = this.stops_array.indexOf(this.startingStation);
+		this.switch_two = this.stops_array.indexOf(this.endingStation);
+		abs_stop = Math.abs(this.switch_one - this.switch_two);
 		return abs_stop;
 	},
 
@@ -34,18 +48,17 @@ var MBTA = {
 				line1_stops,
 				line2_stops;
 
-			this.stops_array1 = this.lines[startingLine];
-			this.stops_array2 = this.lines[endingLine];
-			this.start = stops_array1.indexOf(startingStation);
-			this.switch_one = stops_array1.indexOf('park st');
-			this.switch_two = stops_array2.indexOf('park st');
-			this.stop = stops_array2.indexOf(endingStation);
-			this.line1_stops = Math.abs(start - switch_one);
-			this.line2_stops = Math.abs(stop -switch_two);
+			this.stops_array1 = this.lines[this.startingLine];
+			this.stops_array2 = this.lines[this.endingLine];
+			this.start = stops_array1.indexOf(this.startingStation);
+			this.switch_one = this.stops_array1.indexOf('park st');
+			this.switch_two = this.stops_array2.indexOf('park st');
+			this.stop = this.stops_array2.indexOf(this.endingStation);
+			this.line1_stops = Math.abs(this.start - this.switch_one);
+			this.line2_stops = Math.abs(this.stop - this.switch_two);
 			return (this.line1_stops + this.line2_stops);
 	}
 };
-
 
 
 	// if (startingLine === endingLine) {
