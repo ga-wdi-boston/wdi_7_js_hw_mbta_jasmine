edc29a88098c00ebf571e7be3a4585e345620748
Laure Fischer <laurefischer27@gmail.com>
2014-02-19 20:39:13 -0500

    refactored to include separate constructor for Trip

--- spec/MBTAAppSpec.js
+++ spec/MBTAAppSpec.js
@@ -5,9 +5,15 @@ describe ('MBTAApp', function() {
     'orange': ['Forest Hills', 'Back Bay', 'Chinatown', 'Downtown Crossing', 'State', 'Park St', 'Haymarket Orange', 'North Station']
   };
 
+  beforeEach(function() {
+    trip_1 = new Trip('red', 'orange', 'South Station', 'Back Bay');
+    trip_2 = new Trip('green', 'orange', 'Arlington', 'State');
+  })
+
   describe ('totalStops', function() {
     it("should calculate the total number of stops for a trip", function() {
-      expect(MBTAApp.totalStops(lines, 'red', 'orange', 'South Station', 'Back Bay')).toEqual(5);
+      expect(MBTAApp.totalStops(lines, trip_1)).toEqual(5);
+      expect(MBTAApp.totalStops(lines, trip_2)).toEqual(3);
     });
   });
 });
--- src/MBTAApp.js
+++ src/MBTAApp.js
@@ -5,14 +5,20 @@ var MBTAApp = {
     'orange': ['Forest Hills', 'Back Bay', 'Chinatown', 'Downtown Crossing', 'State', 'Park St', 'Haymarket Orange', 'North Station']
   },
 
-  totalStops: function(lines, startingLine, endingLine, startingStation, endingStation) {
-    var start_index = lines[startingLine].indexOf(startingStation),
-    intersection_index_on_start_line = lines[startingLine].indexOf('Park St'),
-    end_index = lines[endingLine].indexOf(endingStation),
-    intersection_index_on_end_line = lines[endingLine].indexOf('Park St'),
+  totalStops: function(lines, trip) {
+    var start_index = lines[trip.startingLine].indexOf(trip.startingStation),
+    intersection_index_on_start_line = lines[trip.startingLine].indexOf('Park St'),
+    end_index = lines[trip.endingLine].indexOf(trip.endingStation),
+    intersection_index_on_end_line = lines[trip.endingLine].indexOf('Park St'),
     start_distance = Math.abs(start_index - intersection_index_on_start_line),
     end_distance = Math.abs(end_index - intersection_index_on_end_line);
     return start_distance + end_distance;
   }
 };
 
+var Trip = function(startingLine, endingLine, startingStation, endingStation) {
+  this.startingLine = startingLine;
+  this.endingLine = endingLine;
+  this.startingStation = startingStation;
+  this.endingStation = endingStation;
+};
