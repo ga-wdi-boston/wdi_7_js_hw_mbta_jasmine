f4717e2073726ffc0cf4bfdf3fb76c1b6cd994b9
Elisabeth Waltz <elisabeth.waltz@gmail.com>
2014-02-19 23:46:45 -0500

    Specs for start/end line/station added and passing

---
+++ spec/MBTASpec.js
@@ -0,0 +1,18 @@
+describe('mbta', function() {
+  var travel;
+
+  beforeEach(function() {
+    travel = new Trip('red', 'davis', 'orange', 'north station')
+  });
+
+  it('should have both a startingLine and startingStation', function() {
+    expect(travel.startingLine).toEqual('red');
+    expect(travel.startingStation).toEqual('davis');
+  });
+
+  it('should have both an endingLine and endingStation', function() {
+    expect(travel.endingLine).toEqual('orange');
+    expect(travel.endingStation).toEqual('north station');
+  });
+
+})
