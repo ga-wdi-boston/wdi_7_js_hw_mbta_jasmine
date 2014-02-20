5a04c893c4dd9b367ffa83e22f3f1db1262cd5be
Prescott Murphy <prurph@gmail.com>
2014-02-19 22:19:00 -0500

    Refactor Mbta.Trip.prototype into single declaration expression rather than declaring each property/method separately

--- src/Mbta.js
+++ src/Mbta.js
@@ -16,39 +16,39 @@ var Mbta = {
     this.startHere = {line: startingLine, station: startingStation};
     this.endHere = {line: endingLine, station: endingStation};
   }
-}
-
-Mbta.Trip.prototype.lines = {
-  'green':  ["haymarket",
-             "government center",
-             "park street",
-             "boylston",
-             "arlington",
-             "copley"],
-  'red':    ["south station",
-             "park street",
-             "kendall",
-             "central",
-             "harvard",
-             "porter",
-             "davis",
-             "alewife"],
-  'orange': ["north station",
-             "haymarket",
-             "park street",
-             "state",
-             "downtown crossing",
-             "chinatown",
-             "back bay",
-             "forest hills"]
 };
 
-Mbta.Trip.prototype.distanceToPark = function(loc) {
-  var not_park = this.lines[loc.line].indexOf(loc.station),
-      park     = this.lines[loc.line].indexOf("park street");
-  return Math.abs(not_park - park);
+Mbta.Trip.prototype = {
+  lines: {
+           'green':  ["haymarket",
+                      "government center",
+                      "park street",
+                      "boylston",
+                      "arlington",
+                      "copley"],
+           'red':    ["south station",
+                      "park street",
+                      "kendall",
+                      "central",
+                      "harvard",
+                      "porter",
+                      "davis",
+                      "alewife"],
+           'orange': ["north station",
+                      "haymarket",
+                      "park street",
+                      "state",
+                      "downtown crossing",
+                      "chinatown",
+                      "back bay",
+                      "forest hills"]
+  },
+  distanceToPark: function(loc) {
+    var not_park = this.lines[loc.line].indexOf(loc.station),
+        park     = this.lines[loc.line].indexOf("park street");
+    return Math.abs(not_park - park);
+  },
+  tripDistance: function() {
+    return this.distanceToPark(this.startHere) + this.distanceToPark(this.endHere);
+  }
 };
-
-Mbta.Trip.prototype.tripDistance = function() {
-  return this.distanceToPark(this.startHere) + this.distanceToPark(this.endHere);
-}
