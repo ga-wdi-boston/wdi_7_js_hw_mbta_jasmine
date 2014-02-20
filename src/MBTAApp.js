183711516067c4aba1b69953feb13a9e4fc0736f
Laure Fischer <laurefischer27@gmail.com>
2014-02-19 20:06:55 -0500

    refactored to namespace all variables and combine functions into one function for calculating total stops

--- src/MBTAApp.js
+++ src/MBTAApp.js
@@ -1,26 +1,18 @@
 var MBTAApp = {
-  startingLine: prompt("Enter the Starting Line: "),
-  startingStation: prompt("Enter the Starting Station"),
-  endingLine: prompt("Enter the Ending Line"),
-  endingStation: prompt("Enter the Ending Station"),
   lines: {
     'green':['Haymarket Green', 'Government Center', 'Park St', 'Boylston', 'Arlington', 'Copley'],
     'red': ['South Station', 'Park St', 'Kendall', 'Central', 'Harvard', 'Porter', 'Davis', 'Alewife'],
     'orange': ['Forest Hills', 'Back Bay', 'Chinatown', 'Downtown Crossing', 'State', 'Park St', 'Haymarket Orange', 'North Station']
   },
-  distanceToIntersection: function(lines, line, station){
-    var start_index = lines[line].indexOf(station),
-    intersection_index_on_start_line = lines[line].indexOf('Park St');
-    return Math.abs(start_index - intersection_index_on_start_line);
+
+  totalDistance: function(lines, startingLine, endingLine, startingStation, endingStation) {
+    var start_index = lines[startingLine].indexOf(startingStation),
+    intersection_index_on_start_line = lines[startingLine].indexOf('Park St'),
+    end_index = lines[endingLine].indexOf(endingStation),
+    intersection_index_on_end_line = lines[endingLine].indexOf('Park St'),
+    start_distance = Math.abs(start_index - intersection_index_on_start_line),
+    end_distance = Math.abs(end_index - intersection_index_on_end_line);
+    return start_distance + end_distance;
   }
 };
 
-// Display alerts to repeat user input back to user
-alert("Starting at " + MBTAApp.startingLine + " : " + MBTAApp.startingStation);
-alert("Ending at " + MBTAApp.endingLine + " : " + MBTAApp.endingStation);
-
-start_distance = MBTAApp.distanceToIntersection(MBTAApp.lines, MBTAApp.startingLine, MBTAApp.startingStation);
-end_distance = MBTAApp.distanceToIntersection(MBTAApp.lines, MBTAApp.endingLine, MBTAApp.endingStation);
-total_distance = start_distance + end_distance;
-
-console.log(total_distance);
