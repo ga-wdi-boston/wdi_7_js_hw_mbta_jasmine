f9b9dc53275a46c079799c7b2d200e423c7b33c1
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-27 19:34:42 -0400

    created MBTA class, defined lines

---
+++ src/MBTA.js
@@ -0,0 +1,7 @@
+function MBTA() {
+  this.lines = {
+    'green':['haymarket', 'government center', 'park st', 'bolyston', 'arlington', 'copley'],
+    'red': ['south station', 'park st', 'kendall', 'central', 'harvard', 'porter', 'davis', 'alewife'],
+    'orange': ['north station', 'haymarket', 'park st', 'state', 'downtown crossing', 'chinatown', 'back bay', 'forest hills']
+  }
+}
