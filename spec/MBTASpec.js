f15c1ec9c5f6e7284448b6795aca0df261a7c536
Billy1339 <william.m.sargent@gmail.com>
2014-10-27 20:55:03 -0400

    the specRunner has 1 spec, 1 failure

--- spec/MBTASpec.js
+++ spec/MBTASpec.js
@@ -1,9 +1,9 @@
-describe("DistanceToPark", function(line, stop)) {
-  it("calculates the distance to park", function(line, stop)) {
+describe("#DistanceToPark", function(line, stop) {
+  it("calculates the distance to park", function(line, stop) {
     MBTA.DistanceToPark('red', 'harvard');
     MBTA.DistanceToPark('green', 'copley');
 
     expect(MBTA.DistanceToPark('red', 'harvard')).toEqual(3);
     expect(MBTA.DistanceToPark('green', 'copley')).toEqual(3);
-  };
-};
+  });
+});
