f7c3533df161846a06bec57d2a8d38362873bf68
cyurtbil <cenky92@gmail.com>
2014-10-28 00:08:00 -0400

    Added a test for calculation but it is not working, not sure what it is

--- spec/MbtaSpec.js
+++ spec/MbtaSpec.js
@@ -2,4 +2,11 @@ describe("MBTA", function() {
   beforeAction(function() {
     this.mbta = new MBTA("red", "davis", "green", "copley");
   });
+
+  describe("#calculateDistance", function() {
+    it("should calculate the distance between stations traveled", function() {
+      expect(this.mbta.calculateDistance()).toEqual(8);
+    });
+  });
+
 });
