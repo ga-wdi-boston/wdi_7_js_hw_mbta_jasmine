2d766017a15a14e337df52eed2204b3427b161e7
raielin@gmail.com <raielin@gmail.com>
2014-10-27 23:30:04 -0400

    Created line_lookup function to take in a string and pass it in to return the appropriate MBTA line array.

--- spec/mbta.js
+++ spec/mbta.js
@@ -2,54 +2,56 @@ var MBTA;
 
 MBTA = {
   // a bunch of arrays for mbta lines
-  "red": [
-    "Alewife",
-    "Davis",
-    "Porter",
-    "Harvard",
-    "Central",
-    "Kendall/MIT",
-    "Park Street",
-    "South Station"
+  red: [
+    "alewife",
+    "davis",
+    "porter",
+    "harvard",
+    "central",
+    "kendall",
+    "park street",
+    "south station"
   ],
-  "green": [
-    "Haymarket",
-    "Government Center",
-    "Park Street",
-    "Boylston",
-    "Arlington",
-    "Copley"
+  green: [
+    "haymarket",
+    "government center",
+    "park street",
+    "boylston",
+    "arlington",
+    "copley"
   ],
-  "orange": [
-    "North Station",
-    "Haymarket",
-    "Park Street",
-    "State Street",
-    "Downtown Crossing",
-    "Chinatown",
-    "Tufts Medical Center"
+  orange: [
+    "north station",
+    "haymarket",
+    "park street",
+    "state street",
+    "downtown crossing",
+    "chinatown",
+    "tufts medical"
   ],
 
   promptUser: function() {
-    this.startLine = prompt("Please provide your start line.");
+    this.startLine = this.line_lookup(prompt("Please provide your start line."));
     this.startStop = prompt("Please provide your start stop.");
-    this.endLine = prompt("Please provide your end line.");
+    this.endLine = this.line_lookup(prompt("Please provide your end line."));
     this.endStop = prompt("Please provide your end stop.");
   },
 
+  line_lookup: function(lineName) {
+    return this[lineName];
+  },
+
   distance_to_park: function() {
-    this.startDistance = Math.abs(this.startLine.indexOf("Park Street") - this.startLine.indexOf(this.startStop));
-    this.endDistance = Math.abs(this.endLine.indexOf("Park Street") - this.endLine.indexOf(this.endstop));
+    this.startDistance = Math.abs(this.startLine.indexOf("park street") - this.startLine.indexOf(this.startStop));
+    this.endDistance = Math.abs(this.endLine.indexOf("park street") - this.endLine.indexOf(this.endstop));
   },
 
   // prompt user to input starting line, ending line, starting stop, ending stop, etc.
-  calculateDistances: function() {
+  calculateDistance: function() {
     debugger
     return this.startDistance + this.endDistance;
   }
 }
 
-
-
 MBTA.promptUser();
-alert("Your total distance is " + MBTA.calculateDistances() + " stops.");
+alert("Your total distance is " + MBTA.calculateDistance() + " stops.");
