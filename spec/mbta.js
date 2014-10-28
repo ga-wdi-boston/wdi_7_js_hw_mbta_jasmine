fbe7e6938191e217e43efa5da36ee7ca8d825765
raielin@gmail.com <raielin@gmail.com>
2014-10-27 23:52:21 -0400

    Refactored to make all function and variable names in camel case.

--- spec/mbta.js
+++ spec/mbta.js
@@ -31,28 +31,27 @@ MBTA = {
   ],
 
   promptUser: function() {
-    this.startLine = this.line_lookup(prompt("Please provide your start line."));
+    this.startLine = this.lineLookup(prompt("Please provide your start line."));
     this.startStop = prompt("Please provide your start stop.");
-    this.endLine = this.line_lookup(prompt("Please provide your end line."));
+    this.endLine = this.lineLookup(prompt("Please provide your end line."));
     this.endStop = prompt("Please provide your end stop.");
   },
 
-  line_lookup: function(lineName) {
+  lineLookup: function(lineName) {
     return this[lineName];
   },
 
-  distance_to_park: function() {
+  distanceToPark: function() {
     this.startDistance = Math.abs(this.startLine.indexOf("park street") - this.startLine.indexOf(this.startStop));
     this.endDistance = Math.abs(this.endLine.indexOf("park street") - this.endLine.indexOf(this.endstop));
   },
 
   // prompt user to input starting line, ending line, starting stop, ending stop, etc.
-  total_distance: function() {
-    this.distance_to_park();
-    debugger
+  totalDistance: function() {
+    this.distanceToPark();
     return this.startDistance + this.endDistance;
   }
 }
 
 MBTA.promptUser();
-alert("Your total distance is " + MBTA.total_distance() + " stops.");
+alert("Your total distance is " + MBTA.totalDistance() + " stops.");
