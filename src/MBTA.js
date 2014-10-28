185b60d6a2009254a9c73349550963af84a80b21
Billy1339 <william.m.sargent@gmail.com>
2014-10-27 21:43:54 -0400

    mbta.js underway

--- src/MBTA.js
+++ src/MBTA.js
@@ -4,3 +4,23 @@ var MBTA = {
     'red': ['south station', 'park st', 'kendall', 'central', 'harvard', 'porter', 'davis', 'alewife'],
     'orange': ['north station', 'haymarket', 'park st', 'state', 'downtown crossing', 'chinatown', 'back bay', 'forest hills']
   },
+  distanceToPark: function(line, stop) {
+
+  },
+  distance: function(start_line, start_station, stop_line, stop_station) {
+    this.start_line;
+    this.start_station;
+    this.stop_line;
+    this.stop_station;
+    return MBTA.calcDistance
+  },
+  calcDistance: function() {
+    if (this.start_line === this.stop_line) {
+      var totDistance = this.lines[start_line].indexOf(start_station) - this.lines[start_line].indexOf(start_station);
+      return Math.abs(totDistance);
+    } else {
+      var totDistance = distanceToPark(start_line, start_station) + distanceToPark(stop_line, stop_station);
+      return totDistance;
+    }
+  }
+};
