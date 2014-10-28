6637c1e295069cd110b17cc705615aa1bea4a07a
Billy1339 <william.m.sargent@gmail.com>
2014-10-27 21:10:49 -0400

    second test, specRunner has 2 specs, 2 failures

--- spec/MBTASpec.js
+++ spec/MBTASpec.js
@@ -7,3 +7,19 @@ describe("#DistanceToPark", function(line, stop) {
     expect(MBTA.DistanceToPark('green', 'copley')).toEqual(3);
   });
 });
+
+
+describe("#distance", function(start_line, start_station, stop_line, stop_station) {
+  it ("gets the inputs", function(start_line, start_station, stop_line, stop_station){
+    MBTA.distance('red', 'south station', 'orange', 'haymarket');
+    this.start_line;
+    this.start_station;
+    this.stop_line;
+    this.stop_station;
+
+    expect(this.start_line).toEqual('red');
+    expect(this.start_station).toEqual('south station');
+    expect(this.stop_line).toEqual('orange');
+    expect(this.stop_station).toEqual('haymarket');
+  });
+});
