9c14d33103df928493aedc450bf61331c4650ee0
JC Gruta <jase.aturg@gmail.com>
2015-02-18 10:35:37 -0500

    Barely started. Was trying to read the jasmine docs yesterday to understand it more. Will work on this more later.

---
+++ test/spec/mbta_spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+describe('Creating an app for the mbta' function() {
+  describe('creating an object for MBTA lines' function() {
+    it('creates an object for lines',function(){
+      expect(green).toBeDefined();
+    });
+
+  });
+});
