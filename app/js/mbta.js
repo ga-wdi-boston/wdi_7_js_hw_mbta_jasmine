0011a781790a45e3b99ff8debbb760b5f2aa6991
Doug <djmacdonald@uchicago.edu>
2015-02-17 19:02:12 -0500

    Created trip constructor, test passed.

--- app/js/mbta.js
+++ app/js/mbta.js
@@ -2,3 +2,9 @@ var Line = function(name, stops) {
   this.name = name;
   this.stops = stops;
 }
+var Trip = function(startingLine, startingStation, endingLine, endingStation) {
+  this.startingLine = startingLine;
+  this.startingStation = startingStation;
+  this.endingLine = endingLine;
+  this.endingStation = endingStation;
+}
