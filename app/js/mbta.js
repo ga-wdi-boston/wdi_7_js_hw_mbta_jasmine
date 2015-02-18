bb46822d1f9387af99f17ab1447c1c9acce8480a
idelairre <idelairre@gmail.com>
2015-02-18 00:33:39 -0500

    refactored a little bit so that assignment works. Seems to work in the console, turning back to tests.

--- app/js/mbta.js
+++ app/js/mbta.js
@@ -1,21 +1,23 @@
 var MBTA = {
   lines: {
-    "green": ["North Station","Haymarket","Government Center","Park Street","Boylston","Arlington","Copley","Hynes Convention Center", "Kenmore"],
-    "red": ["Alewife","Davis","Porter","Harvard","Central","Kendall/MIT","Charles/MGH","Park Street","Downtown Crossing","South Station"],
-    "orange": ["Oak Grove", "Malden Center", "Wellington", "Assembly","Sullivan Square", "Community College", "North Station","Haymarket","Park Street","State","Downtown Crossing"]
+    "Green": ["North Station","Haymarket","Government Center","Park Street","Boylston","Arlington","Copley","Hynes Convention Center", "Kenmore"],
+    "Red": ["Alewife","Davis","Porter","Harvard","Central","Kendall/MIT","Charles/MGH","Park Street","Downtown Crossing","South Station"],
+    "Orange": ["Oak Grove", "Malden Center", "Wellington", "Assembly","Sullivan Square", "Community College", "North Station","Haymarket","Park Street","State","Downtown Crossing"]
     },
 
-    distance: function(startingLine, endingLine, startingStation, endingStation){
-        findDistance = function(){
+    userInputs: function(startLine, startStation, endLine, endStation){
+      return {
+      startingLine: startLine,
+      endingLine: endLine,
+      startingStation: endLine,
+      endingStation: endStation
+    }
+  },
+    distance: function(){
       if(this.startingLine === this.endingLine){
         return Math.abs(this.lines[this.startingLine].indexOf(startingStation) - this.lines[this.endingLine].indexOf(this.endingStation));
       } else {
       return Math.abs(this.lines[this.startingLine].indexOf(startingStation) - this.lines[this.startingLine].indexOf(this.lines["Park Street"]) + Math.abs(this.lines[this.endingLine].indexOf("Park Street") - this.lines[this.endingLine].indexOf(endingStation)));
     }
-  } return {
-    startingLine: startingLine;
-    endingLine: endingLine;
-    startingStation: startingStation;
-    endingStation: endingStation;
-  };
-}
+  }
+};
