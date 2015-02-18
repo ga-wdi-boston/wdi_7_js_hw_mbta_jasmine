534e7fdb34216832c57dd0c0218029d4ee310aa3
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-02-17 22:50:35 -0500

    modifed test to match program - currently  not passing

--- test/spec/mbta_spec.js
+++ test/spec/mbta_spec.js
@@ -1,6 +1,15 @@
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+describe("MBTA Subway line",function() {
+  // test for variables
+
+  // test for arrays
+  describe("MBTA line arrays",function(){
+    it('Checks for excistance of arrays',function(){
+      expect(green).toEqual("haymarket", "government center", "park st", "bolyston", "arlington", "copley");
+      expect(red[3]).toEqual('kendall');
+      expect(orange[6]).toEqual('chinatown');
+    });
+
   });
+  //test fuction and function's return value
 });
 
