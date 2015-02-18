16ecb1ebda383b586ee807d5c6fd9966a607a43a
KBondanza <kelly.bondanza@gmail.com>
2015-02-18 08:52:58 -0500

    i'm confused, tried to write the tests just like the repo.

--- app/js/mbta.js
+++ app/js/mbta.js
@@ -1,3 +1,36 @@
 var MBTA_line = function(line){
   this.line = line;
 };
+
+MBTA_line.prototype = {
+  describe: function() {
+    return this.line + " has " + indexOf(line) + " stops " + this.connection;
+  }
+};
+
+var MBTA_lineApp = MBTA_lineApp || {};
+
+MBTA_lineApp.green = { line: 'green'};
+
+MBTA_lineApp.createMBTA_line = function(line) {
+  var display = function(){
+    return this.line + " has " + indexOf(line) + " stops " + this.connection;
+  };
+  return {
+    line: line,
+  };
+};
+
+var MBTA = {
+  green:["haymarket", "government center", "park st", "bolyston", "arlington", "copley"],
+  red: ["south station", "park st", "kendall", "central", "harvard", "porter", "davis", "alewife"],
+  orange: ["north station", "haymarket", "park st", "state", "downtown crossing", "chinatown", "back bay", "forest hills"]
+};
+
+var numberStops = function(startLine, startStation, endingLine, endingStation){
+  if(startLine === stopLine) {
+    return Math.abs(MBTA[startLine].indexOf(startStation) - MBTA[stopLine].indexOf(stopStation));
+} else {
+  return Math.abs(MBTA[startLine].indexOf(startStation) - MBTA[startLine].indexOf('Park Street')) + Math.abs(MBTA[stopLine].indexOf('Park Street') - MBTA[stopLine].indexOf(stopStation));
+}
+};
--- test/spec/mbta_spec.js
+++ test/spec/mbta_spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('MBTA_line',function(line){
+describe('MBTA_line',function(){
   it('first of all, it is a function',function(){
     expect(MBTA_line).toEqual(jasmine.any(Function));
     expect(MBTA_line).toBeDefined();
@@ -24,5 +24,62 @@ describe('MBTA_line',function(line){
       expect(green.describe()).toEqual('green line has 6 stops, with Park Street as a connection stop');
       expect(orange.describe()).toEqual('orange line has 8 stops, with Park Street as a connection stop');
     });
+  });
+});
+
+describe('Namespacing in Javascript',function(){
+  describe('MBTA_lineApp namespace',function(){
+    it('creates a new object for namespacing',function(){
+      expect(MBTA_lineApp).toEqual(jasmine.any(Object));
+    });
+    it('has a property called "red" that has an object for a value',function(){
+      expect(MBTA_lineApp.red).toEqual({line: 'red'});
+    });
+    it('has a createMBTA_line property that is a namespaced factory',function(){
+      expect(MBTA_lineApp.createMBTA_line).toEqual(jasmine.any(Function));
+    });
+    it('returns a new object literal from the createMBTA_line factory',function(){
+      expect(MBTA_lineApp.createMBTA_line('red')).toEqual({line: 'red'})
+    });
+    it('has a constructor function called MBTA_line',function(){
+      expect(MBTA_lineApp.MBTA_line).toEqual(jasmine.any(Function));
+    });
+    it('returns the newly constructed objects',function(){
+      expect(green).toEqual(jasmine.any(MBTA_lineApp.MBTA_line));
+      expect(red).toEqual(jasmine.any(MBTA_lineApp.MBTA_line));
+      expect(orange.line).toEqual('orange');
+        expect(green.line).toEqual('green');
+    });
+  });
+});
+
+describe('Two Ways to create a new Object',function(){
+  it('Using an Object constructor',function(){
+    expect(newObjectFromContructor).toEqual({});
+    expect(newObjectFromContructor).toBeDefined();
+  });
+
+  it('Using an Object literal (Preferred)',function(){
+    expect(newObjectFromLiteral).toEqual({});
+    expect(newObjectFromLiteral).toBeDefined();
+  });
+});
+
+describe('The Object.create() method', function(){
+  var alewife = Object.create(Station);
+  var park_street = Object.create(Station, {line: 'orange', 'green', 'red'});
+
+  it('allows inheritance from the Station Object Literal', function(){
+    expect(alewife).toBeDefined();
+    expect(alewife).toEqual(jasmine.any(Object));
+  });
+  it('allows inheritance from the Station Object Literal', function(){
+    expect(red).toBeDefined();
+    expect(red).toEqual(jasmine.any(Object));
+  });
+  it('can override the type property',function(){
+    station.type = "Stations";
+    expect(station.type).toMatch(/Stations/);
+    expect(station.displayType()).toMatch(/Stations/);
   });
 });
