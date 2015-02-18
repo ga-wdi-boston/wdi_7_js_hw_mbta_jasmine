e64c6a04f26ba4aa1e20957628dc5a2c5b04d863
Dan Stoppelman <dstop75@gmail.com>
2015-02-18 01:19:51 -0500

    fix indent

--- app/js/mbta.js
+++ app/js/mbta.js
@@ -1,16 +1,16 @@
 'use strict';
 
 var lines = {
-  'green': ["Haymarket", "Government Center", "Park Street", "Boylston", "Arlington", "Copley"],
-  'red': ["Alewife", "Davis", "Porter", "Harvard", "Central", "Kendall/MIT", "Park Street", "South Station"],
-  'orange': ["North Station", "Haymarket", "Park Street", "State Street", "Downtown Crossing", "Chinatown", "Tufts"]
+    'green': ["Haymarket", "Government Center", "Park Street", "Boylston", "Arlington", "Copley"],
+    'red': ["Alewife", "Davis", "Porter", "Harvard", "Central", "Kendall/MIT", "Park Street", "South Station"],
+    'orange': ["North Station", "Haymarket", "Park Street", "State Street", "Downtown Crossing", "Chinatown", "Tufts"]
 };
 
 var Trip = function(startLine, startStation, endLine, endStation){
-  this.startLine = startLine;
-  this.startStation = startStation;
-  this.endLine = endLine;
-  this.endStation = endStation;
+    this.startLine = startLine;
+    this.startStation = startStation;
+    this.endLine = endLine;
+    this.endStation = endStation;
 };
 
 Trip.prototype = {
