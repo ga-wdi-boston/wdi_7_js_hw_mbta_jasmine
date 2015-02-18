59300f3856ca84a338688e846f377f002e1cffa1
Dan Stoppelman <dstop75@gmail.com>
2015-02-18 00:49:34 -0500

    add Trip constructor

--- app/js/mbta.js
+++ app/js/mbta.js
@@ -3,3 +3,10 @@ var lines = {
   'red': ["Alewife", "Davis", "Porter", "Harvard", "Central", "Kendall/MIT", "Park Street", "South Station"],
   'orange': ["North Station", "Haymarket", "Park Street", "State Street", "Downtown Crossing", "Chinatown", "Tufts"]
 };
+
+var Trip = function(startLine, startStation, endLine, endStation){
+  this.startLine = startLine;
+  this.startStation = startStation;
+  this.endLine = endLine;
+  this.endStation = endStation;
+};
