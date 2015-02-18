fb873077c93e0c81287b2640ac34bb5276f011b9
Nicole Pupillo <nicole.pupillo@gmail.com>
2015-02-17 23:35:49 -0500

    Commented out alerts & console.log for Jasmine testing

--- app/js/mbta.js
+++ app/js/mbta.js
@@ -4,10 +4,11 @@ startingStation,
 endingLine,
 endingStation;
 
-startingLine = prompt("Enter the Starting Line: ");
-startingStation = prompt("Enter the Starting Station");
-endingLine = prompt("Enter the Ending Line");
-endingStation =  prompt("Enter the Ending Station");
+//Prompt user for input
+// startingLine = prompt("Enter the Starting Line: ");
+// startingStation = prompt("Enter the Starting Station");
+// endingLine = prompt("Enter the Ending Line");
+// endingStation =  prompt("Enter the Ending Station");
 
 //hard coded values for testing: 5 total stops
 // startingLine = 'red';
@@ -15,11 +16,11 @@ endingStation =  prompt("Enter the Ending Station");
 // endingLine = 'green';
 // endingStation =  'copley'; // copley index = 5, park st index = 2
 
-console.log("Starting at " + startingLine + " : " + startingStation);
-alert("Starting at " + startingLine + " : " + startingStation);
-
-console.log("Ending at " + endingLine + " : " + endingStation);
-alert("Ending at " + endingLine + " : " + endingStation);
+//User feedback based on their input
+// console.log("Starting at " + startingLine + " : " + startingStation);
+// alert("Starting at " + startingLine + " : " + startingStation);
+// console.log("Ending at " + endingLine + " : " + endingStation);
+// alert("Ending at " + endingLine + " : " + endingStation);
 
 
 // Create a array for each line
@@ -38,5 +39,6 @@ var getStops = function(startingLine, startingStation, endingLine, endingStation
   }
 };
 
-console.log("Number of stops is: " + getStops(startingLine, startingStation, endingLine, endingStation));
-alert("Number of stops is: " + getStops(startingLine, startingStation, endingLine, endingStation));
+//Final output of calculation of the total number of stops
+// console.log("Number of stops is: " + getStops(startingLine, startingStation, endingLine, endingStation));
+// alert("Number of stops is: " + getStops(startingLine, startingStation, endingLine, endingStation));
