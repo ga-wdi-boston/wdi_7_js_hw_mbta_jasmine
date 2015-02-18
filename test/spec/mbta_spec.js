41f8a88de83b511f41b9870752539f9837aec79f
idelairre <idelairre@gmail.com>
2015-02-18 02:04:26 -0500

    one test can't find userInputs as a variable. that is curious, maybe this will lead to why everything passed to it remains undefined.

--- test/spec/mbta_spec.js
+++ test/spec/mbta_spec.js
@@ -1,18 +1,33 @@
 describe("The MBTA", function(){
-  it("will be recognizable as an object", function(){
+  it("determines if it is recognizable as an object", function(){
     expect(MBTA).toBeDefined();
     expect(MBTA).toEqual(jasmine.any(Object));
   });
-  it("will have lines", function(){
+  it("determines if the MBTA has lines", function(){
     expect(MBTA.lines).toBeDefined();
-    expect(MBTA.lines).toEqual(jasmine.any(Object))
+    expect(MBTA.lines).toEqual(jasmine.any(Object));
     expect(MBTA.lines["Red"]).toContain("Alewife","Davis","Porter","Harvard","Central","Kendall/MIT","Charles/MGH","Park Street","Downtown Crossing","South Station");
     expect(MBTA.lines["Green"]).toContain("North Station","Haymarket","Government Center","Park Street","Boylston","Arlington","Copley","Hynes Convention Center", "Kenmore");
     expect(MBTA.lines["Orange"]).toContain("Oak Grove", "Malden Center", "Wellington", "Assembly","Sullivan Square", "Community College", "North Station","Haymarket","Park Street","State","Downtown Crossing");
   });
 });
+
 describe("#userInputs", function(){
-  it("assigns input function arguments to variables", function(){
-    expect(MBTA.userInputs("Red", "South Station", "Green", "Haymarket")).toEqual({Object,args: ["Red", "South Station", "Green", "Haymarket"});
+  it("determines that #userInputs is a function", function(){
+    expect(MBTA.userInputs).toBeDefined();
+    expect(MBTA.userInputs).toEqual(jasmine.any(Function));
+  });
+  it("determines if the method is called", function(){
+    expect(userInputs.calls.any()).toEqual(false);
+
+    MBTA.userInputs();
+
+    expect(MBTA.userInputs.calls.any()).toEqual(true);
+  });
+
+  it("determines if the method assigns input arguments to variables", function(){
+    MBTA.userInputs("Red", "South Station", "Green", "Haymarket");
+
+    expect(MBTA.userInputs.calls.allArgs()).toEqual(["Red", "South Station", "Green", "Haymarket"]);
   });
 });
