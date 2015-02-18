830233789a94cf253033bf214ba1b496a017d92e
Kevin Whelan <Kevinwhelan2098@gmail.com>
2015-02-18 09:43:41 -0500

    Added tests for MBTA

--- app/js/app.js
+++ app/js/app.js
@@ -1,25 +1,20 @@
 'use strict';
-
-var trace = function(){
-  for(var i = 0; i < arguments.length; i++){
-    console.log(arguments[i]);
-  }
-};
-
-var App = App || {};
-
-App.square = function(x){
-  return x * x
-};
-
-App.cube = function(x){
-  return x * x * x
-};
-
-App.greet = function(string){
-  return string = typeof string !== 'undefined' ? 'Hello ' + string : "Hello World";
-};
-
-$(document).ready(function(){
-  trace('hello world');
-});
++var startingLine,
++startingStation,
++endingLine,
++endingStation;
++
++
++lines = {
++  'green':["haymarket", "government center", "park st", "bolyston", "arlington", "copley"],
++  'red': ["south station", "park st", "kendall", "central", "harvard", "porter", "davis", "alewife"],
++  'orange': ["north station", "haymarket", "park st", "state", "downtown crossing", "chinatown", "back bay", "forest hills"]
++};
++
++var numberOfStops = function(startingLine, startingStation, endingLine, endingStation){
++  if (startingLine === endingLine){
++    return Math.abs(lines[startingLine].indexOf(startingStation) - lines[startingLine].indexOf(endingStation));
++  } else {
++    return Math.abs(lines[startingLine].indexOf(startingStation) - lines[startingLine].indexOf('park st')) + Math.abs(lines[endingLine].indexOf(endingStation) - lines[endingLine].indexOf('park st'));
++  };
++};
