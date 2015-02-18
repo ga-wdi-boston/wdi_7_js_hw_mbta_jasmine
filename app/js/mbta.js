1843828dc6f1abc4f39765066c43da107dc83206
Dan Stoppelman <dstop75@gmail.com>
2015-02-18 00:48:38 -0500

    add mbta lines object with green, red, orange lines

---
+++ app/js/mbta.js
@@ -0,0 +1,5 @@
+var lines = {
+  'green': ["Haymarket", "Government Center", "Park Street", "Boylston", "Arlington", "Copley"],
+  'red': ["Alewife", "Davis", "Porter", "Harvard", "Central", "Kendall/MIT", "Park Street", "South Station"],
+  'orange': ["North Station", "Haymarket", "Park Street", "State Street", "Downtown Crossing", "Chinatown", "Tufts"]
+};
